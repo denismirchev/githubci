754f5c8a3abb8b4b4a50bd615b9b42cb070407fd
denismirchev <denis.g.mirchev@gmail.com>
2025-04-03 17:49:53 +0300

    Fixed lint

--- rector.php
+++ rector.php
@@ -7,22 +7,22 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $config): void {
-	$config->paths([
-		__DIR__ . '/tests',
-		__DIR__ . '/add_two_numbers.php',
-	]);
+    $config->paths([
+        __DIR__ . '/tests',
+        __DIR__ . '/add_two_numbers.php',
+    ]);
 
-	$config->importNames();
+    $config->importNames();
 
-	$config->phpstanConfig(__DIR__ . '/phpstan.neon');
+    $config->phpstanConfig(__DIR__ . '/phpstan.neon');
 
-	$config->sets([
-		LevelSetList::UP_TO_PHP_83,
-		SetList::CODE_QUALITY,
-		SetList::EARLY_RETURN,
-		SetList::TYPE_DECLARATION,
-	]);
+    $config->sets([
+        LevelSetList::UP_TO_PHP_83,
+        SetList::CODE_QUALITY,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+    ]);
 
-	$config->disableParallel();
-	$config->cacheDirectory(__DIR__ . '/var/cache/rector');
+    $config->disableParallel();
+    $config->cacheDirectory(__DIR__ . '/var/cache/rector');
 };
